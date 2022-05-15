00be8f0c182ed2cefc9964d133f2598c0f2cc979
David Shapiro <daveshap37@gmail.com>
2022-05-14 20:52:34 -0400

    augmenting the tutor data

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TutorChatbot
 TIM the Tutor Chatbot - an experiment in finetuning GPT-3 to encourage curiosity
+
+## Techniques
+
+- compassionate listening
+- find teachable moments
+- gentle redirection
+- investigate passions
+- encourage curiosity
+- meet the user where they are
+- reference interview
+- recognize and respond to self-disclosure
--- synthesize_convos.py
+++ synthesize_convos.py
@@ -16,7 +16,7 @@ def save_convo(text, topic):
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.85, top_p=1.0, tokens=1000, freq_pen=0.5, pres_pen=0.0, stop=['<<END>>']):  # NOTE: original temp was 0.7 and freq_pen was 0.0 - I turned these up to reduce repetition
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.85, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):  # NOTE: original temp was 0.7 and freq_pen was 0.0 - I turned these up to reduce repetition
     max_retry = 5
     retry = 0
     while True:
@@ -52,7 +52,7 @@ if __name__ == '__main__':
             response = gpt3_completion(prompt)
             outtext = 'User: Hey TIM%s' % response
             print(outtext)
-            tpc = topic.replace(' ', '')[0:15]
+            tpc = topic.replace(' ', '').replace('(','').replace(')','').replace('-','').replace(',','').replace('"')[0:15]
             save_convo(outtext, tpc)
             #exit()
             
